43c25efbafca3cf6c1c007f633cbdd10b6ad8689
Michael Goddard <mgoddard@gopivotal.com>
2017-11-22 10:13:14 -0500

    Fixing the log to append vs. create new

--- start_blockchain_app.sh
+++ start_blockchain_app.sh
@@ -3,5 +3,5 @@
 jar="blockchain-wss-client-0.0.1-SNAPSHOT.jar"
 log_file="blockchain-wss-client.log"
 
-nohup java -Xmx1g -Xms1g -jar $jar </dev/null >$log_file 2>&1 &
+nohup java -Xmx1g -Xms1g -jar $jar </dev/null >>$log_file 2>&1 &
 
